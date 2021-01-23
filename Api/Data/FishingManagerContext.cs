8e3ae1e24b8130bef660968d03462f434125e086
Patrick Tomasi <info@tomasi-developing.ch>
2021-01-23 17:16:37 +0100

    Initial commit

---
+++ Api/Data/FishingManagerContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class FishingManagerContext : DbContext
+    {
+        public FishingManagerContext(DbContextOptions<FishingManagerContext> options) : base(options)
+        {
+        }
+    }
+}
